657f2a46169b367574902e00d9bdbe3905a9feed
ryanodonnell2 <rpodonnell2012@gmail.com>
2022-05-10 17:13:12 -0700

    updated site

--- landing.js
+++ landing.js
@@ -1,40 +1,40 @@
-// const fs = require('fs');
-// const http = require('http');
-// const https = require('https');
-// const privateKey  = fs.readFileSync(__dirname+'\\sslcert\\server.key', 'utf8');
-// const certificate = fs.readFileSync(__dirname+'\\sslcert\\server.cert', 'utf8');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync(__dirname+'\\sslcert\\server.key', 'utf8');
+const certificate = fs.readFileSync(__dirname+'\\sslcert\\server.cert', 'utf8');
 
-// const credentials = {key: privateKey, cert: certificate};
-// const express = require('express');
-// const app = express();
+const credentials = {key: privateKey, cert: certificate};
+const express = require('express');
+const app = express();
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpServer.listen(2442);
+httpsServer.listen(2443, function() {
+    console.log("Server Started!");
+});
+
+// var express = require('express')
+// var fs = require('fs')
+// var https = require('https')
+// var app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('hello world')
 // })
-// // your express configuration here
 
-// var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
-// httpServer.listen(2442);
-// httpsServer.listen(2443, function() {
-//     console.log("Server Started!");
-// });
-
-var express = require('express')
-var fs = require('fs')
-var https = require('https')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
-
-https.createServer({
-  key: fs.readFileSync(__dirname+'/sslcert/server.key'),
-  cert: fs.readFileSync(__dirname+'/sslcert/server.cert')
-}, app)
-.listen(3000, function () {
-  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
-})
+// https.createServer({
+//   key: fs.readFileSync(__dirname+'/sslcert/server.key'),
+//   cert: fs.readFileSync(__dirname+'/sslcert/server.cert')
+// }, app)
+// .listen(3000, function () {
+//   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+// })
